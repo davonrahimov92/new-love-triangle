7894451cd7f4fbf6df4bb0eb5144d8bf61c6f889
davronrahimov9201 <davronrahimov92@gmail.com>
2019-09-05 13:36:52 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,5 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences) {
-    var count = preferences.length;
-    var loveTrianglesCount = 0;
-    for(var i=0;i<count;i++) {
-        if(preferences[i] !== -1) {
-            var i_love = preferences[i];
-            var i_love_love = preferences[i_love-1];
-            var i_love_love_love = preferences[i_love_love-1];
-            if( i + 1!== i_love && i_love_love_love === i + 1) {
-                preferences[i] = preferences[i_love-1] = preferences[i_love_love-1] = -1;
-                loveTrianglesCount++;
-            }
-        }
-    }
-    return loveTrianglesCount;
+    
 };
